30d629da6b8ccb9bb51346bdf4493aa097908c0f
Carlos Guevara <carlosalfredoguevarachinchilla@gmail.com>
2019-08-20 16:50:21 -0600

    se agrego la entidad Escuela, el datacontext y la cadena de conexion en el appsettings

---
+++ AsistenciaComedor/AsistenciaComedor/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using AsistenciaComedor.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AsistenciaComedor.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Escuela> Escuelas { get; set; }
+    }
+}
---
+++ AsistenciaComedor/AsistenciaComedor/Data/Entities/Escuela.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AsistenciaComedor.Data.Entities
+{
+    public class Escuela
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nombre")]
+        [MaxLength(20, ErrorMessage = "EL Campo {0} no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string nombre { get; set; }
+
+        [Display(Name ="Dirección")]
+        [MaxLength(100, ErrorMessage = "EL Campo {0} no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string direccion{ get; set; }
+
+        [Display(Name = "Código de escuela")]
+        [MaxLength(100, ErrorMessage = "EL Campo {0} no puede tener mas de {1} caracteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string codigo { get; set; }
+
+        [Display(Name = "Dirección regional")]
+        [MaxLength(100, ErrorMessage = "EL Campo {0} no puede tener mas de {1} caracteres.")]
+        public string direccionRegional { get; set; }
+    }
+}
